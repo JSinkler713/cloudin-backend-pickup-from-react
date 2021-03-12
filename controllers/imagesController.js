bf00a0fa6ec6cb8d0ee7b042809117a2fc914f2f
James Sinkler <jamessinkler713@gmail.com>
2021-03-12 07:25:22 -0800

    makes a route that works with multer

--- controllers/imagesController.js
+++ controllers/imagesController.js
@@ -5,14 +5,31 @@ const multipart = require('connect-multiparty');
 const multipartMiddleware = multipart();
 const cloudinary = require('cloudinary');
 
+// try with multer as well
+const multer = require('multer');
+const uploads = multer({ dest: '../tmp/uploads/' });
+
 // Databse
-const db = require('../models')
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
+
+router.post('/multer', uploads.single('image'), (req, res)=> {
+  console.log('in the image test route')
+  console.log('req.file', req.file)
+  console.log('this is the req.file.path')
+  console.log(req.file.path)
+  //console.log('this is the req.files.image.path')
+  //console.log(req.files.image.path);
+  console.log('***********************')
+  cloudinary.uploader.upload(req.file.path, function(result) {
+    console.log('from cloudinary', result);
+    res.json({ url: result.secure_url })
+  })
+})
 
 router.post('/', multipartMiddleware, (req, res)=> {
   console.log('in the image test route')
